588f0749dd4fc8f5bff43e34f416c3a9f907212e
Lucas Boberg <lucaspboberg@gmail.com>
2023-05-09 16:47:42 +0200

    Some changes from arvid

--- pages/integritetspolicy.tsx
+++ pages/integritetspolicy.tsx
@@ -38,100 +38,71 @@ const PrivacyPolicy = () => (
       <Box>
         <Container maxW="container.md" py="1rem" fontSize="lg">
           <Text>
-            Privacy Policy for Lunds Akademiska Biljettbyrå (LABB) At LABB, we
-            are committed to protecting the privacy of our users. This Privacy
+            Privacy Policy for Lunds Akademiska Biljettbyrå (LABB). At LABB, we
+            are committed to protecting the privacy of our users. The Privacy
             Policy describes how we collect, use, and share information about
-            you when you use our website and mobile application (the
-            "Services").
+            you when you use our website and mobile application.
             <Heading as="h2" fontSize="2xl" mt="2rem">
               Information We Collect
             </Heading>
-            <Text>
-              We collect information about you when you use our Services. This
-              includes:
-            </Text>
-            <Box pl="2rem" mt="1rem">
+            <VStack mt="1rem" spacing="1rem" alignItems="left">
+              <Text>
+                We collect information when you use our services. This includes:
+              </Text>
               <Text>
                 Personal Information: When you create an account with us, we
                 collect your name and email address. If you purchase a ticket
-                through our Services, our payment provider (Stripe) collect your
-                payment information, which may include your credit card number
-                and billing address. But you will be asked, if you want to save
-                it.
+                through our Services, our payment provider (
+                <Text as="span" color="blue.500">
+                  <a href="https://stripe.com" target="_blank" rel="noreferrer">
+                    Stripe
+                  </a>
+                </Text>
+                ) collects your payment information, which may include your
+                credit card number and billing address. We do not store your
+                credit card, but you have the option to save it for future
+                purchases.
               </Text>
               <Text>
                 Usage Information: We collect information about how you use our
-                Services, such as the pages you visit and the features you use.
+                services, such as the pages you visit and the features you use.
                 We may also collect information about your device, such as your
-                device type, operating system, and browser type.
+                device type, operating system, and what browser you are using.
               </Text>
               <Text>
-                Location Information: If you grant us permission, we may collect
-                your device's location information to provide you with relevant
+                Location Information: If granted permission, we may collect your
+                device&apos;s location information to provide you with relevant
                 content and services.
               </Text>
-            </Box>
+            </VStack>
             <Heading as="h2" fontSize="2xl" mt="2rem">
               How We Use Your Information
             </Heading>
-            <Text>
-              We use your information to provide and improve our Services, as
-              well as for other purposes, such as:
-            </Text>
-            <Box pl="2rem" mt="1rem">
+            <VStack mt="1rem" spacing="1rem" alignItems="left">
               <Text>
-                Personalization: We use your information to personalize your
-                experience on our Services, such as by providing you with
-                personalized recommendations and content.
+                We use your information to provide and improve our services, as
+                well as for other purposes, such as:
               </Text>
               <Text>
-                Marketing: We may use your information to send you marketing
-                communications about our Services, as well as other products and
-                services that may be of interest to you.
-              </Text>
-              <Text>
-                Analytics: We use your information to analyze how our Services
+                Analytics: Collecting information to analyze how our services
                 are used, such as by measuring the effectiveness of our
-                marketing campaigns and improving the user experience.
+                marketing campaigns, improving the user experience and locating
+                app crashes.
               </Text>
-            </Box>
-            <Heading as="h2" fontSize="2xl" mt="2rem">
-              How We Use Your Information
-            </Heading>
-            <Text>
-              We use your information to provide and improve our Services, as
-              well as for other purposes, such as:
-            </Text>
-            <Box pl="2rem" mt="1rem">
-              <Text>
-                Personalization: We use your information to personalize your
-                experience on our Services, such as by providing you with
-                personalized recommendations and content.
-              </Text>
-              <Text>
-                Marketing: We may use your information to send you marketing
-                communications about our Services, as well as other products and
-                services that may be of interest to you.
-              </Text>
-              <Text>
-                Analytics: We use your information to analyze how our Services
-                are used, such as by measuring the effectiveness of our
-                marketing campaigns and improving the user experience.
-              </Text>
-            </Box>
+            </VStack>
             <Heading as="h2" fontSize="2xl" mt="2rem">
               How We Share Your Information
             </Heading>
-            <Text>
-              We do not sell your information to third parties. However, we may
-              share your information with third parties for various purposes,
-              such as:
-            </Text>
-            <Box pl="2rem" mt="1rem">
+            <VStack mt="1rem" spacing="1rem">
+              <Text>
+                We do not sell your information to third party vendors. However,
+                we may share your information with third party vendors for
+                various purposes, such as:
+              </Text>
               <Text>
                 Service Providers: We may share your information with
                 third-party service providers who help us provide and improve
-                our Services, such as payment processors and marketing partners.
+                our services, such as payment processors and marketing partners.
               </Text>
               <Text>
                 Nationer/Sektioner: If you are a member of a Nation/Sektion or
@@ -139,45 +110,44 @@ const PrivacyPolicy = () => (
                 certain information about you with them.
               </Text>
               <Text>
+                Analytics: We may share your information with analytics
+                providers to help us improve our Services.
+              </Text>
+              <Text>
                 Legal Requirements: We may share your information if we are
-                required to do so by law or if we believe that such action is
-                necessary to comply with the law or to protect the rights,
-                property, or safety of our users or others.
+                required to do so by law or if we believe such action is
+                necessary to comply with law or to protect the rights, property,
+                or safety of our users or others.
               </Text>
-            </Box>
+            </VStack>
             <Heading as="h2" fontSize="2xl" mt="2rem">
-              Your Choices
+              Delete Your Information
             </Heading>
-            <Text>
-              You have certain choices about how we use and share your
-              information. You can:
-            </Text>
-            <Box pl="2rem" mt="1rem">
+            <Box mt="1rem">
               <Text>
-                Control Location Information: You can control the collection of
-                location information by adjusting your device's location
-                settings.
-              </Text>
-              <Text>
-                Delete Your Account: You can delete your account at any time by
-                contacting us or in the app when feature is available.
-              </Text>
-              <Text>
-                Analytics: We use your information to analyze how our Services
-                are used, such as by measuring the effectiveness of our
-                marketing campaigns and improving the user experience.
+                You can delete your account at any time by contacting us at{" "}
+                <Text as="span" color="blue.500">
+                  <a href="mailto:info@labbs.se">info@labbs.se</a>
+                </Text>
               </Text>
             </Box>
             <Heading as="h2" fontSize="2xl" mt="2rem">
               Security
             </Heading>
-            <Box pl="2rem" mt="1rem">
+            <Box mt="1rem">
               <Text>
-                We take reasonable measures to protect your information from
-                unauthorized access, use, and disclosure. We do not store
-                passwords; we use OTP. We also do not store payment card details
-                and instead use Stripe. We try to collect only the necessary
-                information.
+                We value our users&apos; privacy and strive to protect your
+                information from unauthorized access, use, and disclosure. We do
+                not store passwords, we use one time passwords instead.
+              </Text>
+              <Text>
+                We do not store any payment card details and instead use{" "}
+                <Text as="span" color="blue.500">
+                  <a href="https://stripe.com" target="_blank" rel="noreferrer">
+                    Stripe
+                  </a>
+                </Text>
+                .
               </Text>
               <Text>
                 However, no method of transmission over the Internet or method
@@ -188,7 +158,7 @@ const PrivacyPolicy = () => (
             <Heading as="h2" fontSize="2xl" mt="2rem">
               Changes to This Privacy Policy
             </Heading>
-            <Box pl="2rem" mt="1rem">
+            <Box mt="1rem">
               <Text>
                 We may update this Privacy Policy from time to time. If we make
                 material changes to this Privacy Policy, we will notify you by
@@ -199,13 +169,13 @@ const PrivacyPolicy = () => (
             <Heading as="h2" fontSize="2xl" mt="2rem">
               Contact Us
             </Heading>
-            <Box pl="2rem" mt="1rem">
+            <Box mt="1rem">
               <Text>
                 If you have any questions or concerns about this Privacy, please
-                contact us: <br />
-                <a href="mailto:info@labbs.se">
-                  <MdMail />
-                </a>
+                contact us at{" "}
+                <Text as="span" color="blue.500">
+                  <a href="mailto:info@labbs.se">info@labbs.se</a>
+                </Text>
               </Text>
             </Box>
           </Text>
